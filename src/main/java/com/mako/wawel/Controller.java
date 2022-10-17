27f39161cbd787c5e4edf0e92c0fa26e717433dd
Jakub Makowiec <jakub.makowiec@outlook.com>
2022-10-17 17:49:57 +0200

    init

--- src/main/java/com/mako/wawel/Controller.java
+++ src/main/java/com/mako/wawel/Controller.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("")
 public class Controller {
     @GetMapping("")
     public String getContent() {
-        return "Content";
+        return "Testowe RestApi kina wawel";
     }
 }
