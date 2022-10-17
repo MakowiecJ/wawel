c9c44a41719cd5a967033f282d6c787947425ed8
Jakub Makowiec <jakub.makowiec@outlook.com>
2022-10-17 17:35:19 +0200

    init

---
+++ src/main/java/com/mako/wawel/Controller.java
@@ -0,0 +1,14 @@
+package com.mako.wawel;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+    @GetMapping("")
+    public String getContent() {
+        return "Content";
+    }
+}
---
+++ src/main/java/com/mako/wawel/WawelApplication.java
@@ -0,0 +1,13 @@
+package com.mako.wawel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WawelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WawelApplication.class, args);
+    }
+
+}
