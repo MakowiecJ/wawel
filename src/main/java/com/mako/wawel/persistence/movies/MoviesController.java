0a41cb87768e0e85a160b6fa7e16cf86a85a4708
Jakub Makowiec <jakub.makowiec@outlook.com>
2022-12-20 12:42:55 +0100

    test

--- src/main/java/com/mako/wawel/persistence/movies/MoviesController.java
+++ src/main/java/com/mako/wawel/persistence/movies/MoviesController.java
@@ -5,6 +5,7 @@ import com.mako.wawel.request.movies.AddMovieRequest;
 import com.mako.wawel.service.movies.MoviesService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,13 +24,13 @@ public class MoviesController {
     }
 
     @PostMapping
-//    @Secured("ROLE_ADMIN")
+    @Secured("ROLE_ADMIN")
     public MovieEntity addMovie(@RequestBody final AddMovieRequest request) {
         return service.addMovie(request);
     }
 
     @DeleteMapping
-//    @Secured("ROLE_ADMIN")
+    @Secured("ROLE_ADMIN")
     public void deleteMovie(@RequestParam final String movieId) {
         service.deleteMovie(movieId);
     }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -24,7 +24,7 @@ CREATE TABLE user_roles(
 
 DROP TABLE IF EXISTS movies;
 CREATE TABLE movies(
-                           ID INTEGER NOT NULL AUTO_INCREMENT,
+                           ID VARCHAR(60) NOT NULL,
                            TITLE VARCHAR(60) NOT NULL,
                            GENRE VARCHAR(60) NOT NULL,
                            MIN_AGE INTEGER NOT NULL,
@@ -44,10 +44,10 @@ INSERT INTO user_roles(USER_ID, ROLE_ID) VALUES
 (1, 1),
 (2, 2);
 
-INSERT INTO movies(TITLE, GENRE, MIN_AGE, DURATION, DESCRIPTION) VALUES
-('Avatar: Istota wody', 'Sci-Fi', 13, 193, 'Akcja filmu Avatar: Istota wody rozgrywa się ponad dziesięć lat po wydarzeniach z pierwszej części. To opowieść o rodzinie Jake’a i Neytiri oraz ich staraniach, by zapewnić bezpieczeństwo sobie i swoim dzieciom, mimo tragedii, których wspólnie doświadczają i bitew, które muszą stoczyć, aby przeżyć.* Drodzy widzowie w filmie Avatar: Istota wody znajduje się kilka scen z dynamicznymi efektami świetlnymi, które mogą powodować dyskomfort u widzów wrażliwych na światło i wpływać na osoby z epilepsją fotogenną.'),
-('Listy do M. 5', 'Komedia Romantyczna', 13, 117, '"Listy do M." powracają z 5. częścią wigilijnej opowieści. W kolejnej odsłonie zobaczymy świąteczne perypetie ulubionych bohaterów. Z czym zmierzą się tym razem? Melowi jak zwykle nic nie wychodzi. Zbieg okoliczności sprawia, że staje się bohaterem mimo woli, a jego nie zawsze kryształowy charakter znowu zostaje wystawiony na próbę. Wojciech, który nie czuje wszechobecnej radosnej atmosfery, spotyka na swojej drodze kogoś, kto zmienia jego świąteczne plany. Z kolei Karina i Szczepan uwikłają się w walkę o spadek, który może poróżnić nawet najbliższych. Przekonają się czy z rodziną rzeczywiście dobrze wychodzi się tylko na zdjęciach. To oczywiście nie wszystko! W tej części pojawią się też nowi bohaterowie i ich zaskakujące historie. "Listy do M. 5" skupią się na uniwersalnych wartościach, takich jak miłość, bliskość czy życzliwość, które obecnie są najistotniejsze.'),
-('Dzika Noc', 'Akcja', 15, 112, 'Producenci filmów ”Nikt”, „John Wick”, „Deadpool 2” realizują dla studia Universal mroczny thriller „Dzika Noc”.
+INSERT INTO movies(ID, TITLE, GENRE, MIN_AGE, DURATION, DESCRIPTION) VALUES
+('12312357', 'Avatar: Istota wody', 'Sci-Fi', 13, 193, 'Akcja filmu Avatar: Istota wody rozgrywa się ponad dziesięć lat po wydarzeniach z pierwszej części. To opowieść o rodzinie Jake’a i Neytiri oraz ich staraniach, by zapewnić bezpieczeństwo sobie i swoim dzieciom, mimo tragedii, których wspólnie doświadczają i bitew, które muszą stoczyć, aby przeżyć.* Drodzy widzowie w filmie Avatar: Istota wody znajduje się kilka scen z dynamicznymi efektami świetlnymi, które mogą powodować dyskomfort u widzów wrażliwych na światło i wpływać na osoby z epilepsją fotogenną.'),
+('32131221', 'Listy do M. 5', 'Komedia Romantyczna', 13, 117, '"Listy do M." powracają z 5. częścią wigilijnej opowieści. W kolejnej odsłonie zobaczymy świąteczne perypetie ulubionych bohaterów. Z czym zmierzą się tym razem? Melowi jak zwykle nic nie wychodzi. Zbieg okoliczności sprawia, że staje się bohaterem mimo woli, a jego nie zawsze kryształowy charakter znowu zostaje wystawiony na próbę. Wojciech, który nie czuje wszechobecnej radosnej atmosfery, spotyka na swojej drodze kogoś, kto zmienia jego świąteczne plany. Z kolei Karina i Szczepan uwikłają się w walkę o spadek, który może poróżnić nawet najbliższych. Przekonają się czy z rodziną rzeczywiście dobrze wychodzi się tylko na zdjęciach. To oczywiście nie wszystko! W tej części pojawią się też nowi bohaterowie i ich zaskakujące historie. "Listy do M. 5" skupią się na uniwersalnych wartościach, takich jak miłość, bliskość czy życzliwość, które obecnie są najistotniejsze.'),
+('87858568', 'Dzika Noc', 'Akcja', 15, 112, 'Producenci filmów ”Nikt”, „John Wick”, „Deadpool 2” realizują dla studia Universal mroczny thriller „Dzika Noc”.
 
 Zobaczymy między innymi gwiazdę serialu "Stranger Things” Davida Harboura oraz zdobywcę nagrody Emmy- Johnego Leguizamo oraz innych: Edi Pattersona, Cam Gigandet Alexa Hassella i Beverly D''Angelo.
 
