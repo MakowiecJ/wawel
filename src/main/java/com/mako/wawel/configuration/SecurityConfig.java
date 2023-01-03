678b3c6e1e11a13b56866f0d821139994095cf82
Jakub Makowiec <jakub.makowiec@outlook.com>
2023-01-03 15:31:51 +0100

    cross origin

--- src/main/java/com/mako/wawel/configuration/SecurityConfig.java
+++ src/main/java/com/mako/wawel/configuration/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .cors().and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "**").permitAll()
                 .antMatchers(HttpMethod.POST, "**").permitAll()
